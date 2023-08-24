de4fd94dfbc1239dcd7554dde4b49a99fc84e4e4
Daniel Praes <danielpraess@gmail.com>
2023-08-24 07:53:05 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/inf2am/IronWorksGym/IronWorksGymApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2am.IronWorksGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronWorksGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronWorksGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/inf2am/IronWorksGym/IronWorksGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2am.IronWorksGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronWorksGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
